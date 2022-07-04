a3120c23dadd7993c3ea9b1b0f6661828682e8ab
SampleUser <pedro.cunha@onfinity.com.br>
2022-07-04 18:45:09 -0300

    Resolve exercise 3

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Exercise 1
 
 function isArray(value) {
+    
     return Array.isArray(value)
 }
 
@@ -12,3 +13,9 @@ function getEnds(list) {
     return [ list[0], list[list.length - 1] ]
 }
 
+// Exercise 3
+
+function getElement(list, index) {
+    
+    return list[index]
+}
